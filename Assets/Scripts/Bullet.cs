d6d8cbec06eb359dbc45ac671ea02be8c5fc54d4
mcoll231 <wisp231@outlook.com>
2023-04-09 19:49:53 -0400

    hp

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
+    public int damage = 20;
     public Rigidbody2D rb;
     public float direction;
     
@@ -12,8 +13,16 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity=transform.right*speed*direction;
+        rb.velocity = transform.right * speed * direction;
 
     }
-    
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
 }
